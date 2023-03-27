e9ad34f891a218d03fc78f3c9312c654e7315407
SitaCicirko <108766560+SitaCicirko@users.noreply.github.com>
2023-03-27 23:58:42 +0100

    3rd Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator c = new Calculator();
+        System.out.println(c.add(2,4));
+        System.out.println(c.subtract(5, 3));
+
     }
 }
