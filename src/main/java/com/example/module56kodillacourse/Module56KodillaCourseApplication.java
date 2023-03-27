af08629467bb731e9ebbb426c76569bb45489f67
SitaCicirko <108766560+SitaCicirko@users.noreply.github.com>
2023-03-27 23:14:27 +0100

    First Commit

---
+++ src/main/java/com/example/module56kodillacourse/Module56KodillaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.module56kodillacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module56KodillaCourseApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hiiii");
+    }
+
+}
---
+++ src/test/java/com/example/module56kodillacourse/Module56KodillaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.module56kodillacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module56KodillaCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
