e251d0b667621d218a80aa85d798c53d6915f46f
SitaCicirko <108766560+SitaCicirko@users.noreply.github.com>
2023-04-04 21:09:51 +0100

    tests

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -3,6 +3,7 @@ package com.kodilla.testing.collection;
 import java.util.*;
 
 public class OddNumbersExterminator {
+
     public List<Integer> exterminate(List<Integer> numbers) {
 
         List<Integer> evenNumbers = new ArrayList<>();
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -8,39 +8,46 @@ import java.util.List;
 
 @DisplayName("Tests for OddNumbersExterminator class")
 class CollectionTestSuite {
-    OddNumbersExterminator exterminator;
 
     @BeforeEach
     public void before() {
         System.out.println("Test Case: begin");
     }
     @AfterEach
-    public void tearDown() {
-        System.out.println("Finishing test...");
+    public void after() {
+        System.out.println("Test Case: end");
+    }
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Test Suite: begin");
+    }
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Test Suite: end");
     }
     @DisplayName("Test filtering odd numbers from an empty list")
     @Test
     public void testOddNumbersExterminatorEmptyList() {
         // Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         List<Integer> emptyList = new ArrayList<>();
 
         // When
-        List<Integer> expectedResult = exterminator.exterminate(emptyList);
-
+        List<Integer> result = oddNumbersExterminator.exterminate(emptyList);
+        System.out.println("Test " + result);
         // Then
-        assertTrue(expectedResult.isEmpty());
+        Assertions.assertEquals(emptyList, result);
     }
 
     @DisplayName("Test filtering odd numbers from a mixed list")
     @Test
     public void testOddNumbersExterminatorNormalList() {
         // Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        OddNumbersExterminator exterminator = new OddNumbersExterminator();
-
 
         // When
-        List<Integer> result = exterminator.exterminate(numbers);
+        List<Integer> result = oddNumbersExterminator.exterminate(numbers);
 
         // Then
         assertEquals(Arrays.asList(2, 4, 6), result);
